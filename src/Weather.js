403779300f96da63a33d5cb29183c9b6add742d5
eFranchesca <112275326+eFranchesca@users.noreply.github.com>
2023-05-20 17:54:24 -0400

    basic styling

--- src/Weather.js
+++ src/Weather.js
@@ -10,7 +10,8 @@ export default function Weather() {
             <input
               type="search"
               placeholder="Enter a city..."
-              className="form-control"/>
+              className="form-control"
+              autoFocus="on"/>
           </div>
           <div className="col-3">
             <input type="submit" value="Search" className="btn btn-success"/>
